cee85967f30f9c835e18d4674c548ec26d118566
ShivamKSah <shivamsah141205@gmail.com>
2025-12-18 23:56:07 +0530

    Remove all files from repository, keep local copies

--- components/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './VitalsChart';
-export * from './VitalsExplainer';
--- components/nurse/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './NurseDashboard';
-export * from './NurseSidebar';
-export * from './InpatientManagementView';
-export * from './AdmissionQueueView';
-export * from './DischargePlanningView';
-export * from './ReportsAnalyticsView';
-export * from './SettingsView';
